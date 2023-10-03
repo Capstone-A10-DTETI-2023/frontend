85e74a705d52b97beed95e0c8b45397a544517b4
saddansyah <saddanakbar@gmail.com>
2023-10-03 10:25:00 +0700

    init: chakraui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from "next/app";
+import { ChakraProvider } from '@chakra-ui/react';
 
 import "@/styles/global.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  )
 };
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,12 @@ import { useRef, useState } from 'react';
 import getConfig from 'next/config';
 
 import Layout from '@/components/Layout';
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react'
 
 const { publicRuntimeConfig } = getConfig();
 const { name } = publicRuntimeConfig.site;
@@ -17,18 +23,20 @@ const Home = () => {
   }
 
   return (
-    <Layout>
-      <section className="h-screen flex items-center bg-white dark:bg-gray-800 transition-all">
-        <div className="container mx-auto prose prose-base w-full px-8">
-          <h1 className='text-center animate-bounce dark:text-white'>{'Capstone A10' || name}</h1>
-          <p className='text-center dark:text-gray-400'>Coming very soon</p>
-          <div className='text-center w-full flex flex-row space-x-3 justify-center'>
-            <button onClick={toggleDarkMode} className='bg-slate-700 dark:bg-white dark:text-slate-800 text-white px-4 py-1 rounded-md'>{text}</button>
-          </div>
-          <hr />
-          <h4 className='w-full bg-amber-100 dark:bg-slate-600 dark:text-white p-4 text-center rounded-lg'>Under construction 👷‍♂️🚧</h4>
+    <Layout>      <section className="h-screen flex items-center bg-white dark:bg-gray-800 transition-all">
+      <div className="container mx-auto prose prose-base w-full px-8">
+        <h1 className='text-center animate-bounce dark:text-white'>{'Capstone A10' || name}</h1>
+        <p className='text-center dark:text-gray-400'>Coming very soon</p>
+        <div className='text-center w-full flex flex-row space-x-3 justify-center'>
+          <button onClick={toggleDarkMode} className='bg-slate-700 dark:bg-white dark:text-slate-800 text-white px-4 py-1 rounded-md'>{text}</button>
         </div>
-      </section>
+        <hr />
+        <Alert status='info' variant={'solid'} flexDirection={'column'} rounded={'lg'}>
+          <AlertIcon />
+          This is alert test
+        </Alert>
+      </div>
+    </section>
     </Layout>
   );
 };
